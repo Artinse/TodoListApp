2e8f68eb6f39ea1e0d32697220dff40ebc8ac516
Artinse <artinse@mail.ru>
2021-12-16 20:12:40 +0300

    Add task delete functional

--- script.js
+++ script.js
@@ -29,8 +29,93 @@ const tasks = [
   },
 ];
 
-
-
 (function (tasksArray) {
-	console.log(tasksArray);
+  const tasksObj = tasksArray.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  // UI elements
+  const tasksContainer = document.querySelector('.tasks-list');
+  const todoForm = document.forms['todoForm'];
+  const inputTitle = todoForm.elements['title'];
+  const inputBody = todoForm.elements['body'];
+  renderTasks(tasksObj);
+
+  // Events
+  todoForm.addEventListener('submit', onFormSubmitHandler);
+  tasksContainer.addEventListener('click', onDeleteHandler);
+  function renderTasks(tasksList) {
+    if (!tasksList) return console.error('TasksList not defined');
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach((task) => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    tasksContainer.appendChild(fragment);
+  }
+  function listItemTemplate({ _id, title, body } = {}) {
+    const li = document.createElement('li');
+    li.setAttribute('data-task-id', _id);
+    li.classList.add('task-item');
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.classList.add('task-list-title');
+    const descr = document.createElement('p');
+    descr.textContent = body;
+    descr.classList.add('task-list-descr');
+    const button = document.createElement('button');
+    button.classList.add('deleteBtn');
+    button.textContent = 'Delete';
+    li.append(span, descr, button);
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+    if (!titleValue || !bodyValue) {
+      alert('Please, write title and body for task');
+      return;
+    }
+    const newTask = createNewTask(titleValue, bodyValue);
+    const newList = listItemTemplate(newTask);
+    tasksContainer.insertAdjacentElement('afterbegin', newList);
+    todoForm.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
+    tasksObj[newTask._id] = newTask;
+    return { ...newTask };
+  }
+  function deleteTask(id) {
+		const {title} = tasksObj[id];
+    const userConfirm = confirm(`You want delete this task = ${title}? `);
+		if (!userConfirm) return userConfirm;
+		delete tasksObj[id];
+		return userConfirm;
+  }
+
+	function deleteTaskForomHTML(confirmed, taskItem) {
+		if (!confirmed) return;
+		taskItem.remove();
+	}
+
+  function onDeleteHandler({ target }) {
+    if (!target.classList.contains('deleteBtn')) {
+      return;
+    } else {
+      const taskItem = target.closest('[data-task-id]');
+      const id = taskItem.dataset.taskId;
+      const confirmed = deleteTask(id);
+			deleteTaskForomHTML(confirmed, taskItem);
+    }
+  }
 })(tasks);
